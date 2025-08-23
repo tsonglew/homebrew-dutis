64438c1906cc985826d8779df4400d9ff2832b82
Tsonglew <tsonglew@gmail.com>
2025-08-23 23:59:08 +0800

    Update dutis.rb

--- Formula/dutis.rb
+++ Formula/dutis.rb
@@ -5,7 +5,7 @@ class Dutis < Formula
   desc "A cmd util to manage default app on macOS, inspired by duti"
   homepage "https://github.com/tsonglew/dutis"
   url "https://github.com/tsonglew/dutis/archive/v2.0.0.tar.gz"
-  sha256 "163e558603fd3e1293c55d7cd8d9e674ce52ea8e2861a280c8467a8d2c6e6a63"
+  sha256 "35be115c3236a3954251da5a28d9a2d4b82e2e81e68e48f295d7566bef02fd27"
   license "MIT"
 
   depends_on "rust" => :build
