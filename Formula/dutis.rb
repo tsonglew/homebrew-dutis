bea2de66b002005f4a415b2ec7fbfa1ec0a8e303
Tsonglew <tsonglew@gmail.com>
2025-08-23 23:58:36 +0800

    Update dutis.rb

--- Formula/dutis.rb
+++ Formula/dutis.rb
@@ -5,37 +5,18 @@ class Dutis < Formula
   desc "A cmd util to manage default app on macOS, inspired by duti"
   homepage "https://github.com/tsonglew/dutis"
   url "https://github.com/tsonglew/dutis/archive/v2.0.0.tar.gz"
-  sha256 "33a7d03c19ed77acadfda8a3eeb73ec67a9fb31aa9d820d97e2f08d85515178c"
-  license ""
+  sha256 "163e558603fd3e1293c55d7cd8d9e674ce52ea8e2861a280c8467a8d2c6e6a63"
+  license "MIT"
 
-  # depends_on "cmake" => :build
   depends_on "rust" => :build
   depends_on "duti"
 
-
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
-
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test dutis`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    # Test that the binary can run and show help
+    system "#{bin}/dutis", "--help"
   end
 end
